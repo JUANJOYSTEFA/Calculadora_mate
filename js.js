b3ed86fb3679a2e8e98dd4fae0a332d4dfdda65e
Juan José <juanjoseromerogomez0@gmail.com>
2023-09-10 17:04:55 -0500

    iniciamos el proyecto

---
+++ js.js
@@ -0,0 +1,7 @@
+function dmsToDd() {
+    let grados = document.getElementById("grados").value;
+    let minutos = document.getElementById("minutos").value;
+    let segundos = document.getElementById("segundos").value;
+    var dd = degrees + minutes / 60 + seconds / 3600;
+
+}
