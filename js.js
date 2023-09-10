f925073d6e5defd4a67b48859f1ea4c1f0872825
Juan José <juanjoseromerogomez0@gmail.com>
2023-09-10 17:29:16 -0500

    Se hace la primera funcion que es pasar de GMS a G

--- js.js
+++ js.js
@@ -2,10 +2,10 @@
 //git remote add origin https://github.com/JUANJOYSTEFA/Calculadora_mate.git
 //git branch -M main
 //git push -u origin main
-function dmsToDd() {
-    let grados = document.getElementById("grados").value;
-    let minutos = document.getElementById("minutos").value;
-    let segundos = document.getElementById("segundos").value;
-    var dd = degrees + minutes / 60 + seconds / 3600;
-
+function gmsagrados() {
+    let grados = parseFloat(document.getElementById("grados").value);
+    let minutos = parseFloat(document.getElementById("minutos").value);
+    let segundos = parseFloat(document.getElementById("segundos").value);
+    var resultado = grados + minutos / 60 + segundos / 3600;
+    document.getElementById("resultado").innerHTML = resultado;
 }
