7a143039330a7fbc4d6491903b8edd43864f0bf9
Juan José <juanjoseromerogomez0@gmail.com>
2023-09-26 20:21:04 -0500

    agrego index

--- index.php
+++ index.php
@@ -1,2 +1,32 @@
-<?php
-header("location:gms/index.php");
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="gms/style.css">
+    <title>Grados Decimales -> GMS</title>
+</head>
+
+<body>
+    <div class="menu">
+        <a href="/calculadora_mate/grados/"><span>Grados Decimales->GMS</span></a><br><br>
+        <a href="/calculadora_mate/gms/"><span>GMS->Grados Decimales</span></a><br><br>
+        <a href="/calculadora_mate/grds-rad/"><span>Grados->Radianes</span></a><br><br>
+        <a href="/calculadora_mate/rad-grds/"><span>Radianes->Grados</span></a><br><br>
+        <a href="/calculadora_mate/arco/"><span>Calcular longitud de arco</span></a><br><br>
+        <a href="/calculadora_mate/area/"><span>Calcular Área del sector circular</span></a><br><br>
+        <a href="/calculadora_mate/vangular/"><span>Calcular Velocidad Angular</span></a><br><br>
+        <a href="/calculadora_mate/vlineal/"><span>Calcular Velocidad Lineal</span></a><br><br>
+    </div>
+    <div class="pagina">
+        <div class="wrapper">
+            <center>
+                <h1>Iniciar la Calculadora</h1>
+                <a href="gms/index.php"><button class="submit">Iniciar</button></a>
+            </center>
+        </div>
+    </div>
+</body>
+
+</html>
